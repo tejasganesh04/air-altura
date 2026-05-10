fdb14bd708dcba6a0e43b66ac449cb190e294b97
Tejas Ganesh <tejasganesh323@gmail.com>
2026-05-10 16:58:53 +0530

    fix: add production env to API-Gateway Sequelize config

--- API-Gateway/src/config/database-config.js
+++ API-Gateway/src/config/database-config.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
-module.exports = {
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT || 'mysql',
-    },
+const config = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host:     process.env.DB_HOST,
+    port:     parseInt(process.env.DB_PORT) || 3306,
+    dialect:  process.env.DB_DIALECT || 'mysql',
+    logging:  false,
 };
+
+module.exports = { development: config, production: config };
